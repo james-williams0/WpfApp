7249d68c820e4660218bacd38a4385a2104639f0
James Williams <j.a.williams373@gmail.com>
2020-06-01 22:50:09 +0100

    Introducting navigation options and (some) dependency injection.

---
+++ WpfApp/ViewModel/DataVisualiserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfApp.ViewModel.Helper;
+using WpfApp.ViewModel.Interfaces;
+
+namespace WpfApp.ViewModel
+{
+    public class DataVisualiserViewModel : OnPropertyChangedImplementation, INavigatable
+    {
+        public void OnNavigateAway()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigateTo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp/ViewModel/NotepadViewModel.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using WpfApp.ViewModel.Helper;
+using WpfApp.ViewModel.Interfaces;
+
+namespace WpfApp.ViewModel
+{
+    public class NotepadViewModel : OnPropertyChangedImplementation, INavigatable
+    {
+        public bool Bold { get; set; } = false;
+
+        public bool Underline { get; set; } = false;
+
+        public bool Italic { get; set; } = false;
+
+        private SolidColorBrush boldBackground = Brushes.LightGray;
+
+        public SolidColorBrush BoldBackground
+        {
+            get { return boldBackground; }
+            set
+            {
+                boldBackground = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private SolidColorBrush underlineBackground = Brushes.LightGray;
+
+        public SolidColorBrush UnderlineBackground
+        {
+            get { return underlineBackground; }
+            set
+            {
+                underlineBackground = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private SolidColorBrush italicBackground = Brushes.LightGray;
+
+        public SolidColorBrush ItalicBackground
+        {
+            get { return italicBackground; }
+            set
+            {
+                italicBackground = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private RichTextBox mainRTBProperty;
+
+        public RichTextBox MainRTBProperty
+        {
+            get { return mainRTBProperty; }
+            set
+            {
+                mainRTBProperty = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NotepadViewModel(RichTextBox mainRTB)
+        {
+            mainRTBProperty = mainRTB;
+        }
+
+        public ICommand BoldToggle
+        {
+            get { return new RelayCommand<object>(p => ToggleBold()); }
+        }
+
+        public void ToggleBold()
+        {
+            Bold = !Bold;
+            EditingCommands.ToggleBold.Execute(null, mainRTBProperty);
+            if (Bold)
+            {
+                BoldBackground = Brushes.DarkGray;
+            }
+            else
+            {
+                BoldBackground = Brushes.LightGray;
+            }
+        }
+
+        public ICommand UnderlineToggle
+        {
+            get { return new RelayCommand<object>(p => ToggleUnderline()); }
+        }
+
+        public void ToggleUnderline()
+        {
+            Underline = !Underline;
+            EditingCommands.ToggleUnderline.Execute(null, mainRTBProperty);
+            if (Underline)
+            {
+                UnderlineBackground = Brushes.DarkGray;
+            }
+            else
+            {
+                UnderlineBackground = Brushes.LightGray;
+            }
+        }
+
+        public ICommand ItalicToggle
+        {
+            get { return new RelayCommand<object>(p => ToggleItalic()); }
+        }
+
+        public void ToggleItalic()
+        {
+            Italic = !Italic;
+            EditingCommands.ToggleItalic.Execute(null, mainRTBProperty);
+            if (Italic)
+            {
+                ItalicBackground = Brushes.DarkGray;
+            }
+            else
+            {
+                ItalicBackground = Brushes.LightGray;
+            }
+        }
+
+        public void OnNavigateTo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigateAway()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
